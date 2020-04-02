cfce47f9542e54c4d2506478a0ae27b2cd26e587
Antonio <mendezruiz20@gmail.com>
2020-04-02 14:38:38 +0200

    Añadimos algo en las views

--- Código/Proyecto/principal/models.py
+++ Código/Proyecto/principal/models.py
@@ -1,9 +1,1 @@
 from django.db import models
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-
-
-def scrapping_stack(busqueda):
-    html = urlopen("https://es.stackoverflow.com")
-    bsobj = BeautifulSoup(html.read())
-    return bsobj.h1
--- Código/Proyecto/principal/views.py
+++ Código/Proyecto/principal/views.py
@@ -1,4 +1,13 @@
 from django.views.generic import TemplateView
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 class PrincipalView(TemplateView):
     template_name = 'index.html'
+
+def scrapping_stack(busqueda):
+    html = urlopen("https://google.com")
+    bsobj = BeautifulSoup(html.read())
+    print(bsobj.h1)
+
+
