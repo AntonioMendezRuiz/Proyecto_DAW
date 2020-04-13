77c8cee781576beb1c52ee547f3f306677481501
Antonio <mendezruiz20@gmail.com>
2020-04-13 13:22:29 +0200

    implementa formularios

--- Código/Proyecto/Proyecto/urls.py
+++ Código/Proyecto/Proyecto/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from principal.views import PrincipalView
+from principal.views import PrincipalView, RespuestasView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', PrincipalView.as_view(), name="home"),
+    path('consulta/', RespuestasView.as_view(), name="consulta"),
 ]
--- Código/Proyecto/principal/views.py
+++ Código/Proyecto/principal/views.py
@@ -1,13 +1,33 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+@csrf_exempt
+
+def scrapping(request):
+    if request.method == 'POST':
+        busqueda = request.POST.get('busca')
+        html = urlopen("https://google.com/" + busqueda)
+        bsobj = BeautifulSoup(html.read())
+        bsobj1 = bsobj.title
+        template = loader.get_template("respuestas.html")
+
+        return HttpResponse(template.render(bsobj1, request))
+
+
+
+class RespuestasView(TemplateView):
+    template_name = 'respuestas.html'
+
+
 class PrincipalView(TemplateView):
     template_name = 'index.html'
 
-def scrapping_stack(busqueda):
-    html = urlopen("https://google.com")
-    bsobj = BeautifulSoup(html.read())
-    print(bsobj.h1)
+        
+        
 
+    
 
